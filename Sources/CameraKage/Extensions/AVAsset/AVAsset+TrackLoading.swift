75343b23b4e082a232be970940788ab018bded63
Andrei <lobont.andrei1@gmail.com>
2023-07-07 10:30:50 +0300

    Track loading for older versions

--- Sources/CameraKage/Extensions/AVAsset/AVAsset+TrackLoading.swift
+++ Sources/CameraKage/Extensions/AVAsset/AVAsset+TrackLoading.swift
@@ -10,16 +10,27 @@ import AVFoundation
 extension AVAsset {
     func getTrack(for mediaTrack: AVMediaType,
                   completion: @escaping((Result<AVAssetTrack?, CompressionError>) -> Void)) {
-        loadTracks(withMediaType: mediaTrack) { tracksList, error in
-            guard error == nil else {
-                completion(.failure(.failedToLoadTrackToCompress))
-                return
+        if #available(iOS 15.0, *) {
+            loadTracks(withMediaType: mediaTrack) { tracksList, error in
+                guard error == nil else {
+                    completion(.failure(.failedToLoadTrackToCompress))
+                    return
+                }
+                guard let tracksList, let firstTrack = tracksList.first else {
+                    completion(.success(nil))
+                    return
+                }
+                completion(.success(firstTrack))
             }
-            guard let tracksList, let firstTrack = tracksList.first else {
-                completion(.success(nil))
-                return
+        } else {
+            DispatchQueue.global().async { [weak self] in
+                guard let self else { return }
+                guard let firstTrack = self.tracks(withMediaType: mediaTrack).first else {
+                    completion(.success(nil))
+                    return
+                }
+                completion(.success(firstTrack))
             }
-            completion(.success(firstTrack))
         }
     }
 }
